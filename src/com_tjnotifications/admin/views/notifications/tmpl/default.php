26b43e1794642f614df3a3d6ad9c9a97fc26621e
pranoti_p <pranoti_p@techjoomla.com>
2017-02-22 12:53:19 +0530

    Lanaguage file changes and hidden field add

--- src/com_tjnotifications/admin/views/notifications/tmpl/default.php
+++ src/com_tjnotifications/admin/views/notifications/tmpl/default.php
@@ -172,6 +172,7 @@ $listDirn      = $this->escape($this->state->get('list.direction'));
 
 	<input type="hidden" name="task" value=""/>
 	<input type="hidden" name="boxchecked" value="0"/>
+	<input type="hidden" name="extension" value="<?php echo $this->component; ?>" />
 	<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
 	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
 	<?php echo JHtml::_('form.token'); ?>
