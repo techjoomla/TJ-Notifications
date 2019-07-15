d67bb18bf6663908eb9c5280e47216fedee9aaf6
Amol <thite-amol@users.noreply.github.com>
2019-07-15 17:04:15 +0530

    Task #37 chore: Update the doc comment (#49)
    * Task #37 chore: Update the doc comment
    
    * Task #37 chore: Update the doc comment

--- src/com_tjnotifications/site/controllers/preferences.php
+++ src/com_tjnotifications/site/controllers/preferences.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * @package     TJNotification
- * @subpackage  com_tjnotification
+ * @package     TJNotifications
+ * @subpackage  com_tjnotifications
  *
+ * @author      Techjoomla <extensions@techjoomla.com>
  * @copyright   Copyright (C) 2009 - 2019 Techjoomla. All rights reserved.
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
