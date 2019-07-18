525d59e3c3cfcc1cdaefde9908612284188955fc
Manoj L <manojLondhe@users.noreply.github.com>
2019-07-18 18:15:51 +0530

    Update 1.0.4.sql

--- src/com_tjnotifications/admin/sql/updates/mysql/1.0.4.sql
+++ src/com_tjnotifications/admin/sql/updates/mysql/1.0.4.sql
@@ -1,10 +1,8 @@
-
-
-
 --
 -- Change default table engine to InnoDB;
 -- Necessary for indexes to work
 --
+
 ALTER TABLE `#__tj_notification_providers` ENGINE = InnoDB;
 ALTER TABLE `#__tj_notification_templates` ENGINE = InnoDB;
 ALTER TABLE `#__tj_notification_user_exclusions` ENGINE = InnoDB;
@@ -14,5 +12,3 @@ ALTER TABLE `#__tj_notification_providers` CONVERT TO CHARACTER SET utf8mb4 COLL
 ALTER TABLE `#__tj_notification_user_exclusions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 SET FOREIGN_KEY_CHECKS = 1;
 ALTER TABLE `#__tj_notification_templates` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-
