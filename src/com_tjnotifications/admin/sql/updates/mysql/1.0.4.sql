b7e49e58eb8fd6d19378ce52d69f4e8cacf263f5
Vijay Khollam <vijay_k@tekditechnologies.com>
2019-07-18 11:56:42 +0530

    Update SQL and version #51

--- src/com_tjnotifications/admin/sql/updates/mysql/1.0.4.sql
+++ src/com_tjnotifications/admin/sql/updates/mysql/1.0.4.sql
@@ -1,8 +1,5 @@
 
---
--- Change default table engine to InnoDB;
--- Necessary for indexes to work
---
+
 
 --
 -- Change default table engine to InnoDB;
@@ -12,6 +9,10 @@ ALTER TABLE `#__tj_notification_providers` ENGINE = InnoDB;
 ALTER TABLE `#__tj_notification_templates` ENGINE = InnoDB;
 ALTER TABLE `#__tj_notification_user_exclusions` ENGINE = InnoDB;
 
+--
+-- Change default table engine to InnoDB;
+-- Necessary for indexes to work
+--
 ALTER TABLE `#__tj_notification_providers` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE `#__tj_notification_templates` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE `#__tj_notification_user_exclusions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
