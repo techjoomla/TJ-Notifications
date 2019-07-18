3eb1596e896024445ea9c141a84c0a4d18a870e0
Vijay Khollam <vijay_k@tekditechnologies.com>
2019-07-18 16:04:28 +0530

    Update SQL and version #51

--- src/com_tjnotifications/admin/sql/updates/mysql/1.0.4.sql
+++ src/com_tjnotifications/admin/sql/updates/mysql/1.0.4.sql
@@ -9,8 +9,10 @@ ALTER TABLE `#__tj_notification_providers` ENGINE = InnoDB;
 ALTER TABLE `#__tj_notification_templates` ENGINE = InnoDB;
 ALTER TABLE `#__tj_notification_user_exclusions` ENGINE = InnoDB;
 
+SET FOREIGN_KEY_CHECKS = 0;
 ALTER TABLE `#__tj_notification_providers` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE `#__tj_notification_templates` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE `#__tj_notification_user_exclusions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SET FOREIGN_KEY_CHECKS = 1;
+ALTER TABLE `#__tj_notification_templates` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 
