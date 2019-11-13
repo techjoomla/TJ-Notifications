319e376336f85f69b77310471ac5dd0359e108d7
Deepa <deepa_g@techjoomla.com>
2019-11-13 14:50:10 +0530

    Bug #152597 fix: JGive pkg is not installing : Error SQL Specified key was too long; max key length is 1000 bytes

--- src/com_tjnotifications/admin/sql/updates/mysql/1.0.6.sql
+++ src/com_tjnotifications/admin/sql/updates/mysql/1.0.6.sql
@@ -1,3 +1,3 @@
 ----Change table Key;
 
-ALTER TABLE `#__tj_notification_user_exclusions` CHANGE KEY `client1` (`client`(100),`provider`(50),`key`(100));
+ALTER TABLE `#__tj_notification_user_exclusions` ADD INDEX `client1` (`client`(100), `provider`(50), `key`(100)), DROP INDEX `client1`;
