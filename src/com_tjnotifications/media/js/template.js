e6f88cfb81570eaa283a1fd4ab434c2e9336f6a2
poonam_b <poonam_b@techjoomla.com>
2019-12-24 17:40:26 +0530

    Preview email facility in tjnotification

--- src/com_tjnotifications/media/js/template.js
+++ src/com_tjnotifications/media/js/template.js
@@ -6,26 +6,21 @@
  * @copyright   Copyright (C) 2009 - 2019 Techjoomla. All rights reserved.
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
-
 var template = {
-
-	previewTemplate: function () {
-
-	var url = new URL(window.location.href);
-	var id= url.searchParams.get('id');
-
-	jQuery(document).on('click', 'button[data-target="#templatePreview"]', function () {
-
-			jQuery.ajax({
-				url: Joomla.getOptions('system.paths').base + "/index.php?option=com_tjnotifications&task=notification.getSampleData&id=" + id,
-				type: 'GET',
-				success: function(data) {
-
-						jQuery("#previewTempl").append(data);
-				},
-				error: function(xhr, ajaxOptions, thrownError) {}
-			})
-
-	});
-	}
-}
+    previewTemplate: function() {
+        var e = new URL(window.location.href).searchParams.get("id");
+        jQuery(document).on("click", 'button[data-target="#templatePreview"]', function() {
+            jQuery.ajax({
+                url: Joomla.getOptions("system.paths").base + "/index.php?option=com_tjnotifications&task=notification.getSampleData&id=" + e,
+                type: "GET",
+                data: {
+                    data: jQuery("#jform_email_body").serialize()
+                },
+                success: function(e) {
+                    jQuery("#previewTempl").append(e)
+                },
+                error: function(e, t, a) {}
+            })
+        })
+    }
+};
