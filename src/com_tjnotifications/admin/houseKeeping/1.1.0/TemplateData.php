aeb55c03b712bb3bb8a6f7e8e94c536bb9757311
Kishori <kishori_k@techjoomla.com>
2020-03-26 17:33:54 +0530

    Task #156108 chore: Migration of existing Template

--- src/com_tjnotifications/admin/houseKeeping/1.1.0/TemplateData.php
+++ src/com_tjnotifications/admin/houseKeeping/1.1.0/TemplateData.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * @version    SVN: <svn_id>
- * @package    SocialAds
- * @author     Techjoomla <extensions@techjoomla.com>
- * @copyright  Copyright (c) 2009-2019 TechJoomla. All rights reserved.
- * @license    GNU General Public License version 2 or later.
+ * @package     TJNotifications
+ * @subpackage  com_tjnotifications
+ *
+ * @author      Techjoomla <extensions@techjoomla.com>
+ * @copyright   Copyright (C) 2009 - 2019 Techjoomla. All rights reserved.
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
 // No direct access
