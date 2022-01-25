33b420117ddb1253cb2031667e023f04b589c833
Ankush Maherwal <ankush_m@techjoomla.com>
2022-01-25 15:16:26 +0530

    Task #173069 feat: Update infra extensions code used in JGive to make Joomla 4 compatible

--- src/com_tjnotifications/site/views/preferences/tmpl/default_bs3.php
+++ src/com_tjnotifications/site/views/preferences/tmpl/default_bs3.php
@@ -29,7 +29,7 @@ Factory::getDocument()->addScriptDeclaration('
 
 	function addPreferance(pId,client,provider,key)
 	{
-		if(pId)
+		if (pId)
 		{
 			jQuery.ajaxSetup({
 				global: false,
@@ -70,7 +70,7 @@ Factory::getDocument()->addScriptDeclaration('
 	}
 	function removePreferance(pId,client,provider,key)
 	{
-		if(pId)
+		if (pId)
 		{
 			jQuery.ajaxSetup({
 				global: false,
@@ -119,118 +119,116 @@ Factory::getDocument()->addScriptDeclaration('
 	</div>
 
 	<div id="display_info"></div>
-		<ul class="nav nav-tabs">
+	<ul class="nav nav-tabs">
 		<?php if (!empty($this->clients)) : ?>
 		<?php foreach ($this->clients as $i => $menu) :?>
-			<li>
-				<a data-toggle="tab" aria-controls="<?php echo($menu->client); ?>"
-					href="<?php echo('#'.$menu->client); ?>">
-					<?php echo str_replace("com_","",$menu->client); ?>
-				</a>
-			</li>
+		<li>
+			<a data-toggle="tab" aria-controls="<?php echo($menu->client); ?>" href="<?php echo('#'.$menu->client); ?>">
+				<?php echo str_replace("com_","",$menu->client); ?>
+			</a>
+		</li>
 		<?php endforeach; ?>
 		<?php endif; ?>
-		</ul>
+	</ul>
 
 	<div class="tab-content">
-	<?php foreach ($this->clients as $i => $menu) :?>
-		<div class="tab-pane" id="<?php echo($menu->client); ?>" >
+		<?php foreach ($this->clients as $i => $menu) :?>
+		<div class="tab-pane" id="<?php echo($menu->client); ?>">
 			<table class="table table-striped table-hover">
 				<thead>
 					<tr>
-					<th width="20%">
+						<th width="20%">
 
-					</th>
-					<?php if (!empty($this->providers)) : ?>
+						</th>
+						<?php if (!empty($this->providers)) : ?>
 						<?php foreach ($this->providers as $i => $head) :?>
-							<th width="30%">
-									<?php echo($head->provider); ?>
-							</th>
+						<th width="30%">
+							<?php echo($head->provider); ?>
+						</th>
 						<?php endforeach; ?>
-					<?php endif; ?>
+						<?php endif; ?>
 					</tr>
 				</thead>
 				<tbody>
 					<div class="row">
 						<div class="col-md-3">
-						<?php foreach($this->keys[$menu->client] as $key=>$values) : ?>
-							<?php foreach($values as $value ) : ?>
+							<?php foreach ($this->keys[$menu->client] as $key=>$values) : ?>
+							<?php foreach ($values as $value ) : ?>
 							<tr>
 								<td align="center">
 									<?php echo $value; ?>
 								</td>
 								<?php foreach ($this->providers as $i => $head) :?>
 								<td>
-								<?php  $temp=0; ?>
-								<?php foreach ($this->adminPreferences[$head->provider] as $adminKey => $admin) :?>
-									<?php if($admin->client == $menu->client && $admin->key == $value) : $temp++; ?>
+									<?php  $temp=0; ?>
+									<?php foreach ($this->adminPreferences[$head->provider] as $adminKey => $admin) :?>
+									<?php if ($admin->client == $menu->client && $admin->key == $value) : $temp++; ?>
 
-										<?php if(empty($this->preferences)) :  ?>
+									<?php if (empty($this->preferences)) :  ?>
 
-											<div class="control">
-												<fieldset class="btn-group btn-group-yesno radio pull-left" >
-													<input type="radio" id="<?php echo $value.$i; ?>" name="prefer" value="1" onclick="removePreferance('<?php echo $value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')" checked="checked" />
-													<input type="radio" id="<?php echo $key.$i; ?>" name="prefer1" value="0" onclick="addPreferance('<?php echo $value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')" />
-													<label class="btn-success" for="<?php echo $value.$i; ?>" ><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_ENABLE'); ?></label>
-													<label class="btn" for="<?php echo $key.$i; ?>" ><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_DISABLE'); ?></label>
-												</fieldset>
-											</div>
+									<div class="control">
+										<fieldset class="btn-group btn-group-yesno radio pull-left">
+											<input type="radio" id="<?php echo $value.$i; ?>" name="prefer" value="1" onclick="removePreferance('<?php echo $value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')" checked="checked" />
+											<input type="radio" id="<?php echo $key.$i; ?>" name="prefer1" value="0" onclick="addPreferance('<?php echo $value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')" />
+											<label class="btn-success" for="<?php echo $value.$i; ?>"><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_ENABLE'); ?></label>
+											<label class="btn" for="<?php echo $key.$i; ?>"><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_DISABLE'); ?></label>
+										</fieldset>
+									</div>
 
-											<div class="pull-right" id="<?php echo 'ajax-loader'.$value.$i; ?>"  ></div>
-											<div class="pull-right" id="<?php echo 'tick'.$value.$i; ?>"  ></div>
-										<?php else: $count=0; ?>
+									<div class="pull-right" id="<?php echo 'ajax-loader'.$value.$i; ?>"></div>
+									<div class="pull-right" id="<?php echo 'tick'.$value.$i; ?>"></div>
+									<?php else: $count=0; ?>
 
-										<?php foreach ($this->preferences as $j => $prefer) :?>
-											<?php if($prefer->client == $menu->client && $prefer->key == $value && $prefer->provider == $head->provider) : ?>
-											<?php $count++; ?>
-											<div class="control">
-												<fieldset class="btn-group btn-group-yesno radio pull-left" >
-														<input type="radio" id="<?php echo $menu->client.$value.$i; ?>" name="prefer" value="1" onclick="removePreferance('<?php echo $menu->client.$value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')"  />
-														<input type="radio" id="<?php echo $menu->client.$key.$i; ?>" name="prefer1" value="0" onclick="addPreferance('<?php echo $menu->client.$value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')" checked="checked" />
-														<label class="btn" for="<?php echo $menu->client.$value.$i; ?>" ><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_ENABLE'); ?></label>
-														<label class="btn-danger" for="<?php echo $menu->client.$key.$i; ?>" ><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_DISABLE'); ?></label>
-												</fieldset>
-											</div>
-											<?php endif;?>
-										<?php endforeach; ?>
+									<?php foreach ($this->preferences as $j => $prefer) :?>
+									<?php if ($prefer->client == $menu->client && $prefer->key == $value && $prefer->provider == $head->provider) : ?>
+									<?php $count++; ?>
+									<div class="control">
+										<fieldset class="btn-group btn-group-yesno radio pull-left">
+											<input type="radio" id="<?php echo $menu->client.$value.$i; ?>" name="prefer" value="1" onclick="removePreferance('<?php echo $menu->client.$value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')" />
+											<input type="radio" id="<?php echo $menu->client.$key.$i; ?>" name="prefer1" value="0" onclick="addPreferance('<?php echo $menu->client.$value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')" checked="checked" />
+											<label class="btn" for="<?php echo $menu->client.$value.$i; ?>"><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_ENABLE'); ?></label>
+											<label class="btn-danger" for="<?php echo $menu->client.$key.$i; ?>"><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_DISABLE'); ?></label>
+										</fieldset>
+									</div>
+									<?php endif;?>
+									<?php endforeach; ?>
 
-											<?php if ($count==0): ?>
-												<div class="control">
-													<fieldset class="btn-group btn-group-yesno radio pull-left" >
-														<input type="radio" id="<?php echo $menu->client.$value.$i; ?>" name="prefer" value="1" onclick="removePreferance('<?php echo $menu->client.$value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')"  checked="checked" />
-														<input type="radio" id="<?php echo $menu->client.$key.$i; ?>" name="prefer1" value="0"  onclick="addPreferance('<?php echo $menu->client.$value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')" />
-														<label class="btn-success" for="<?php echo $menu->client.$value.$i; ?>" ><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_ENABLE'); ?></label>
-														<label class="btn" for="<?php echo $menu->client.$key.$i; ?>" ><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_DISABLE'); ?></label>
-													</fieldset>
-												</div>
-											<?php endif; ?>
+									<?php if ($count==0): ?>
+									<div class="control">
+										<fieldset class="btn-group btn-group-yesno radio pull-left">
+											<input type="radio" id="<?php echo $menu->client.$value.$i; ?>" name="prefer" value="1" onclick="removePreferance('<?php echo $menu->client.$value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')" checked="checked" />
+											<input type="radio" id="<?php echo $menu->client.$key.$i; ?>" name="prefer1" value="0" onclick="addPreferance('<?php echo $menu->client.$value.$i; ?>','<?php echo($menu->client); ?>','<?php echo($head->provider); ?>','<?php echo $value; ?>')" />
+											<label class="btn-success" for="<?php echo $menu->client.$value.$i; ?>"><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_ENABLE'); ?></label>
+											<label class="btn" for="<?php echo $menu->client.$key.$i; ?>"><?php echo Text::_('COM_TJNOTIFICATION_VIEWS_PREFERENCES_FIELD_DISABLE'); ?></label>
+										</fieldset>
+									</div>
+									<?php endif; ?>
 
-										<?php endif;?>
+									<?php endif;?>
 
 									<?php endif;?>
 
 									<?php endforeach; ?>
 
 									<?php if ($temp == 0): ?>
-										<span class="label label-warning"><?php echo Text::_('COM_TJNOTIFICATIONS_VIEW_PREFERENCES_TAB_UNSUBSCRIBED'); ?></span>
+									<span class="label label-warning"><?php echo Text::_('COM_TJNOTIFICATIONS_VIEW_PREFERENCES_TAB_UNSUBSCRIBED'); ?></span>
 									<?php endif; ?>
-									<div class="pull-right" id="<?php echo 'ajax-loader'.$menu->client.$value.$i; ?>" ></div>
-									<div class="pull-right" id="<?php echo 'tick'.$menu->client.$value.$i; ?>" ></div>
-									</td>
+									<div class="pull-right" id="<?php echo 'ajax-loader'.$menu->client.$value.$i; ?>"></div>
+									<div class="pull-right" id="<?php echo 'tick'.$menu->client.$value.$i; ?>"></div>
+								</td>
 								<?php endforeach; ?>
 							</tr>
 							<?php endforeach; ?>
-						<?php endforeach; ?>
+							<?php endforeach; ?>
 						</div>
 					</div>
 				</tbody>
 			</table>
 		</div>
-	<?php endforeach;?>
+		<?php endforeach;?>
 	</div>
 
-	<input type="hidden" name="task" value=""/>
-	<input type="hidden" name="boxchecked" value="0"/>
+	<input type="hidden" name="task" value="" />
+	<input type="hidden" name="boxchecked" value="0" />
 	<?php echo HTMLHelper::_('form.token'); ?>
 </form>
-
