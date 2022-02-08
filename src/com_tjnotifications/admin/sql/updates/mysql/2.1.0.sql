dc11ea2b622b63de901a9095ef6d57e1d0d5c689
ttpllt46 <praneet_s@tekditechnologies.com>
2022-02-08 18:38:10 +0530

    Issue #179295 task: Add webhook support for Shika/Vowel in TjNotifications

--- src/com_tjnotifications/admin/sql/updates/mysql/2.1.0.sql
+++ src/com_tjnotifications/admin/sql/updates/mysql/2.1.0.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `#__tj_notification_template_configs` ADD COLUMN `webhook_url` text DEFAULT NULL AFTER `body`;
-ALTER TABLE `uusig_tj_notification_logs` ADD COLUMN `webhook_url` TEXT NULL DEFAULT NULL AFTER `body`;
+ALTER TABLE `#__tj_notification_logs` ADD COLUMN `webhook_url` TEXT NULL DEFAULT NULL AFTER `body`;
