c61849f6c8e8711c890912e4c1c66e769d360b8c
vishal adheli <vishal.adheli@tekditechnologies.com>
2025-04-15 16:35:45 +0530

    code indentation

--- src/com_tjnotifications/install.tjnotifications.php
+++ src/com_tjnotifications/install.tjnotifications.php
@@ -133,32 +133,32 @@ class Com_TjnotificationsInstallerScript
 
 
 	private function addMissingColumns()
-    {
-        $db = Factory::getDbo();
-        $columns = $this->getTableColumns('#__tj_notification_template_configs');
-        if (!array_key_exists('webhook_url', $columns)) {
-            $db->setQuery("ALTER TABLE `#__tj_notification_template_configs` ADD COLUMN `webhook_url` text DEFAULT NULL AFTER `body`;");
-            $db->execute();
-        }
-        if (!array_key_exists('use_global_webhook_url', $columns)) {
-            $db->setQuery("ALTER TABLE `#__tj_notification_template_configs` ADD COLUMN `use_global_webhook_url` TINYINT(1) NOT NULL DEFAULT '1' COMMENT 'Use Global Config Webhook URLs' AFTER `webhook_url`;");
-            $db->execute();
-        }
+	{
+		$db = Factory::getDbo();
+		$columns = $this->getTableColumns('#__tj_notification_template_configs');
+		if (!array_key_exists('webhook_url', $columns)) {
+			$db->setQuery("ALTER TABLE `#__tj_notification_template_configs` ADD COLUMN `webhook_url` text DEFAULT NULL AFTER `body`;");
+			$db->execute();
+		}
+		if (!array_key_exists('use_global_webhook_url', $columns)) {
+			$db->setQuery("ALTER TABLE `#__tj_notification_template_configs` ADD COLUMN `use_global_webhook_url` TINYINT(1) NOT NULL DEFAULT '1' COMMENT 'Use Global Config Webhook URLs' AFTER `webhook_url`;");
+			$db->execute();
+		}
 		$columns = $this->getTableColumns('#__tj_notification_logs');
 		if (!array_key_exists('webhook_url', $columns)) {
-            $db->setQuery("ALTER TABLE `#__tj_notification_logs` ADD COLUMN `webhook_url` TEXT NULL DEFAULT NULL AFTER `body`;");
-            $db->execute();
-        }
-    }
+			$db->setQuery("ALTER TABLE `#__tj_notification_logs` ADD COLUMN `webhook_url` TEXT NULL DEFAULT NULL AFTER `body`;");
+			$db->execute();
+		}
+	}
 
 	private function getTableColumns($table)
-    {
-        $db = Factory::getDbo();
-        $db->setQuery("SHOW COLUMNS FROM `$table`");
-        $columns = $db->loadColumn(0);
+	{
+		$db = Factory::getDbo();
+		$db->setQuery("SHOW COLUMNS FROM `$table`");
+		$columns = $db->loadColumn(0);
 
-        return array_flip($columns);
-    }
+		return array_flip($columns);
+	}
 
 	/**
 	 * method to run before an install/update/uninstall method
